57b989294118faea24bbd299b62c6ac3c7172e14
Krzysztof W <45880989+iProXDPL@users.noreply.github.com>
2025-04-02 10:49:15 +0200

    zmiany

--- window.py
+++ window.py
@@ -5,27 +5,35 @@ import subprocess
 import os
 import time
 import json
+import uuid
 
+random_id = str(uuid.uuid4())  
 VENV_PATH = r".\venv\Scripts\activate.bat"
+checking_status = True  # Flaga do kontrolowania pętli sprawdzania statusu
 
 def check_rasa_status():
+    global checking_status
+    if not checking_status:
+        return
+    
     try:
         response = requests.get("http://localhost:5005")
         if response.status_code == 200:
             update_status("BOT Online", "green")
+            checking_status = False  # Zatrzymanie dalszego sprawdzania statusu
+            return
         else:
             update_status("BOT Offline", "red")
     except requests.exceptions.RequestException:
         update_status("BOT Offline", "red")
     
-    root.after(5000, check_rasa_status)
+    root.after(5000, check_rasa_status)  # Kontynuacja sprawdzania jeśli bot jest offline
 
 def update_status(status_text, color):
     status_label.config(text=status_text, fg=color)
 
 def start_rasa():
     global rasa_process, actions_process
-    # Uruchamiamy rasa run i rasa run actions w nowych oknach CMD
     rasa_process = subprocess.Popen(
         ['cmd.exe', '/K', f'{VENV_PATH} && rasa run'], creationflags=subprocess.CREATE_NEW_CONSOLE
     )
@@ -33,8 +41,9 @@ def start_rasa():
         ['cmd.exe', '/K', f'{VENV_PATH} && rasa run actions'], creationflags=subprocess.CREATE_NEW_CONSOLE
     )
 
-
 def stop_rasa():
+    global checking_status
+    checking_status = False  # Zatrzymanie sprawdzania statusu
     if rasa_process:
         subprocess.call(['taskkill', '/F', '/T', '/PID', str(rasa_process.pid)])
     if actions_process:
@@ -49,15 +58,14 @@ def send_message():
     user_input.delete(0, tk.END)
     
     response = requests.post(
-        "http://localhost:5005/webhooks/rest/webhook",  # Adres API Rasa Chatbot
-        json={"sender": "user", "message": user_message}
+        "http://localhost:5005/webhooks/rest/webhook",
+        json={"sender": random_id, "message": user_message}
     )
     
     bot_response = ""
     suggestions = []
     if response.status_code == 200:
         messages = response.json()
-        print(messages)
         for message in messages:
             if "text" in message:
                 bot_response += message["text"] + "\n"
@@ -79,19 +87,18 @@ def update_suggestions(new_suggestions):
         widget.destroy()
     
     if not new_suggestions:
-        new_suggestions = ["Cześć!", "Jaka jest pogoda?"]
+        new_suggestions = ["Witam", "Szukam połaczenia"]
     
     for suggestion in new_suggestions:
         btn = tk.Button(suggestions_frame, text=suggestion, command=lambda s=suggestion: insert_suggestion(s))
         btn.pack(side=tk.LEFT, padx=2)
-
 
 start_rasa()
 
 # Tworzenie GUI
 root = tk.Tk()
 root.title("Chatbot Rasa")
-root.protocol("WM_DELETE_WINDOW", lambda: (root.destroy(),stop_rasa()))  # Zamknięcie Rasa i GUI
+root.protocol("WM_DELETE_WINDOW", lambda: (root.destroy(), stop_rasa()))  # Zamknięcie Rasa i GUI
 
 status_label = tk.Label(root, text="BOT Offline", fg="red")
 status_label.pack(pady=5)
